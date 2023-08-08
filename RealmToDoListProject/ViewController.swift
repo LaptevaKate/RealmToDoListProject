efaf16725874dd4bdf621e0728f0f9dbb48eca18
LaptevaKate <ekaterina213@yandex.ru>
2023-08-08 13:08:14 +0300

    Initial Commit

---
+++ RealmToDoListProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmToDoListProject
+//
+//  Created by Екатерина Лаптева on 8.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
